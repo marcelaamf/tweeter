96ffad2ebaab70047334e1654a7e3dd4997241aa
Marcela Fernandes <mam.fernandes@hotmail.com>
2023-03-03 22:29:59 -0700

    add ajax post an get request for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,31 @@
 
 $(document).ready(function(){
   console.log("client is ready");
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ]
 
-// Create  new tweet
+// Create New Tweet
   const createTweetElement = function(tweet){
     const escapeText = function (str) {
       let div = document.createElement('div');
@@ -38,18 +38,18 @@ $(document).ready(function(){
         return div.innerHTML;
     };
 
-    let tweetMsg = $(
+    let $tweetMsg = $(
       `<article class="tweets-container">
         <header>
           <div class="avatar-name">
-            <img class ="avatar" src="${escapeText(data.avatars)}">
+            <img class ="avatar" src="${escapeText(tweet.user.avatars)}">
             <p>${escapeText(tweet.user.name)}</p>   
           </div> 
           <p>${escapeText(tweet.user.handle)}</p>
         </header>
-          <p>tweet text</p>
+          <p class="tweets-texts">${escapeText(tweet.content.text)}</p>
         <footer>
-          <p>${escapeText(tweet.created_at)}</p>
+          <p>${escapeText(timeago.format(`${tweet.created_at} days ago`))}</p>
           <div class ="tweet-icons">
             <i class="fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -58,18 +58,46 @@ $(document).ready(function(){
         </footer>      
       </article>`
     );
-    return tweetMsg;
+    return $tweetMsg;
   }
-// render tweets
+// Render Tweets
   const renderTweets = function(tweetsDataBase) {
     for (let tweet in tweetsDataBase)
-    $('.posted-tweets').append(createTweetElement(tweetsDataBase[tweet]));
+    $('.posted-tweets').prepend(createTweetElement(tweetsDataBase[tweet]));
    
   }
+  renderTweets();
 
-  renderTweets(data)
 
+//Submit New Tweets
+  
+    const submitForm = $('form')
+    submitForm.on('submit', function(event) {
+      event.preventDefault();
+      const data = $(this).serialize();
+      const textArea = $("tweet-text");
+ 
+      $.ajax({
+        method: 'POST',
+        url: "/tweets",
+        data: data,
+        success: function() { 
+          loadTweets();
+        }
+      });
+    });
+   
+ 
+//Load new tweets
+  const loadTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: "/tweets",
+    })  
+    .then((response) => {
+          renderTweets(response);
+    })
+  };
+  loadTweets();
 
 });
-
-
